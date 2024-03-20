3d9cd9b8c396b9c47c09ecc16021a6870082557f
jens07nj <jakejensen07@icloud.com>
2024-03-20 12:36:09 +1100

    first commit - added print hello world
    2nd commit - made loop that prints as many times as wanted with random amount of 'o'

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world')
+import random
+x = random.randint(1 , 20)
+
+print(f'hell{'o' * x} world')
+ans = input('Print again?')
+while ans != '':
+    x = random.randint(1, 20)
+    print(f'hell{'o' * x} world')
+    ans = input('Print again?')
